2af01744dfb5ff50b6813ab424ed51ef307ffec6
AngelNg23 <angel230797@gmail.com>
2021-08-04 12:18:48 -0500

    Se sube proyecto

---
+++ src/main/java/com/telcel/dashboard/service/FoliosSDService.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.telcel.dashboard.service;
+
+
+import com.telcel.dashboard.entity.FoliosSD;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Angel
+ */
+public interface FoliosSDService {
+    
+    public void getDetalleXLSX(HttpServletResponse response, String fechaFin)throws IOException;
+    
+}
---
+++ src/main/java/com/telcel/dashboard/serviceImpl/GenericReportingService.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.telcel.dashboard.serviceImpl;
+
+
+public class GenericReportingService {
+
+//    @Autowired
+//    private JdbcTemplate jdbc;
+//
+//    public byte[] exportPdfFile (Map<String, Object> params, URL path) throws SQLException, JRException, IOException {
+//	Connection conn = conect();
+//	JasperReport jasper = (JasperReport) JRLoader.loadObject(path);
+//	JasperPrint print = JasperFillManager.fillReport(jasper, params, conn);
+//	conn.close();
+//	try {
+//	    return JasperExportManager.exportReportToPdf(print);
+//           
+//	} catch (JRException e) {
+//	    log.error(e.getMessage());
+//	    throw new RuntimeException(e);
+//	}
+//    }
+//
+//    private Connection conect () throws SQLException {
+//	return jdbc.getDataSource().getConnection();
+//    }
+
+}
